74d7dcdc182171639e01b6a2dff0859996ef6614
Ivana Setiawan <ivana.setiawan@gmail.com>
2014-04-23 16:06:50 +0200

    First try

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,15 @@
 require([
   '$api/models',
+  'scripts/toplist',
   'scripts/language-example',
   'scripts/cover-example',
   'scripts/button-example',
   'scripts/playlist-example'
-], function(models, languageExample, coverExample, buttonExample, playlistExample) {
+], function(models, toplist, languageExample, coverExample, buttonExample, playlistExample) {
   'use strict';
 
+  toplist.doTopList();
+  
   languageExample.doHelloWorld();
   coverExample.doCoverForAlbum();
   buttonExample.doShareButtonForArtist();
